1d11d3e3ed9781e93d2aa67eba0d1345916498e0
Atif Aziz <code@raboof.com>
2020-12-22 21:27:22 +0100

    Revert unrelated change to "Ref.cs"

--- src/CSharpMinifierConsole/Ref.cs
+++ src/CSharpMinifierConsole/Ref.cs
@@ -1,4 +1,4 @@
-#region Copyright (c) 2014 Atif Aziz. All rights reserved.
+﻿#region Copyright (c) 2014 Atif Aziz. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a
 // copy of this software and associated documentation files (the "Software"),
