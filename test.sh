7aae6f2287f31e61ed497cae8245bf6e3b5cd980
Atif Aziz <code@raboof.com>
2021-12-28 13:40:21 +0100

    Log test results & publish during CI builds

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -e
 cd "$(dirname "$0")"
 dotnet tool restore
 ./build.sh
-dotnet test --no-build --collect:"XPlat Code Coverage" -c Debug tests
-dotnet test --no-build --collect:"XPlat Code Coverage" -c Release tests
+dotnet test --no-build --collect:"XPlat Code Coverage" --logger:nunit -c Debug tests
+dotnet test --no-build --collect:"XPlat Code Coverage" --logger:nunit -c Release tests
 dotnet reportgenerator "-reports:tests/TestResults/*/coverage.cobertura.xml" -targetdir:tmp -reporttypes:TextSummary
 cat tmp/Summary.txt
