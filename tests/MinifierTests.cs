035391b56e08ae41684956fdb34a7cc2e292a1b2
Atif Aziz <code@raboof.com>
2026-01-02 20:52:36 +0100

    Use raw string for minification test source

--- tests/MinifierTests.cs
+++ tests/MinifierTests.cs
@@ -44,64 +44,66 @@ public class MinifierTests
     [Test]
     public void Minify()
     {
-        const string source = @"
-#line 1
-# line 1
-/* https://unlicense.org/
- *
- * This is free and unencumbered software released into the public domain.
- *
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
- *
- * In jurisdictions that recognize copyright laws, the author or authors
- * of this software dedicate any and all copyright interest in the
- * software to the public domain. We make this dedication for the benefit
- * of the public at large and to the detriment of our heirs and
- * successors. We intend this dedication to be an overt act of
- * relinquishment in perpetuity of all present and future rights to this
- * software under copyright law.
- *
- * THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- *
- * For more information, please refer to <http://unlicense.org>
- */
+        const string source = """
+            #line 1
+            # line 1
+            /* https://unlicense.org/
+             *
+             * This is free and unencumbered software released into the public domain.
+             *
+             * Anyone is free to copy, modify, publish, use, compile, sell, or
+             * distribute this software, either in source code form or as a compiled
+             * binary, for any purpose, commercial or non-commercial, and by any
+             * means.
+             *
+             * In jurisdictions that recognize copyright laws, the author or authors
+             * of this software dedicate any and all copyright interest in the
+             * software to the public domain. We make this dedication for the benefit
+             * of the public at large and to the detriment of our heirs and
+             * successors. We intend this dedication to be an overt act of
+             * relinquishment in perpetuity of all present and future rights to this
+             * software under copyright law.
+             *
+             * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+             * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+             * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+             * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+             * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+             * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+             * OTHER DEALINGS IN THE SOFTWARE.
+             *
+             * For more information, please refer to <http://unlicense.org>
+             */
 
-#region!
-#! // not really valid
-#endregion!
+            #region!
+            #! // not really valid
+            #endregion!
 
-#region Imports
-using System;
-#endregion
+            #region Imports
+            using System;
+            #endregion
 
-static class Program
-{
-    static void Main()
-    {
-        const string alphabetText =
-#if UPPER
-            ""THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.""
-#else // !UPPER
-            ""The quick brown fox jumps over the lazy dog.""
-#endif
-            ;
+            static class Program
+            {
+                static void Main()
+                {
+                    const string alphabetText =
+            #if UPPER
+                        "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."
+            #else // !UPPER
+                        "The quick brown fox jumps over the lazy dog."
+            #endif
+                        ;
 
-        Console.WriteLine(@""
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Quisque ut sem massa.
-            In at fringilla ipsum.
-            Phasellus ut urna pretium felis porttitor euismod mattis sed augue."");
-    }
-}";
+                    Console.WriteLine(@"
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Quisque ut sem massa.
+                        In at fringilla ipsum.
+                        Phasellus ut urna pretium felis porttitor euismod mattis sed augue.");
+                }
+            }
+
+            """;
 
         var minified = Minifier.Minify(NormalizeLineEndings(source), "\n")
                                .ToDelimitedString(string.Empty);
@@ -135,60 +137,62 @@ static class Program
     [Test]
     public void MinifyWithCommentFilter()
     {
-        const string source = @"
-/* https://unlicense.org/
- *
- * This is free and unencumbered software released into the public domain.
- *
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
- *
- * In jurisdictions that recognize copyright laws, the author or authors
- * of this software dedicate any and all copyright interest in the
- * software to the public domain. We make this dedication for the benefit
- * of the public at large and to the detriment of our heirs and
- * successors. We intend this dedication to be an overt act of
- * relinquishment in perpetuity of all present and future rights to this
- * software under copyright law.
- *
- * THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- *
- * For more information, please refer to <http://unlicense.org>
- */
+        const string source = """
+            /* https://unlicense.org/
+             *
+             * This is free and unencumbered software released into the public domain.
+             *
+             * Anyone is free to copy, modify, publish, use, compile, sell, or
+             * distribute this software, either in source code form or as a compiled
+             * binary, for any purpose, commercial or non-commercial, and by any
+             * means.
+             *
+             * In jurisdictions that recognize copyright laws, the author or authors
+             * of this software dedicate any and all copyright interest in the
+             * software to the public domain. We make this dedication for the benefit
+             * of the public at large and to the detriment of our heirs and
+             * successors. We intend this dedication to be an overt act of
+             * relinquishment in perpetuity of all present and future rights to this
+             * software under copyright law.
+             *
+             * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+             * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+             * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+             * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+             * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+             * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+             * OTHER DEALINGS IN THE SOFTWARE.
+             *
+             * For more information, please refer to <http://unlicense.org>
+             */
 
-#region Imports
-using System;
-#endregion
+            #region Imports
+            using System;
+            #endregion
 
-    //! This is a VERY IMPORTANT comment!
+                //! This is a VERY IMPORTANT comment!
 
-static class Program
-{
-    /*! internal */ static void Main()
-    {
-        const string alphabetText =
-#if UPPER
-            ""THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.""
-#else // !UPPER
-            ""The quick brown fox jumps over the lazy dog.""
-#endif
-            ;
+            static class Program
+            {
+                /*! internal */ static void Main()
+                {
+                    const string alphabetText =
+            #if UPPER
+                        "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."
+            #else // !UPPER
+                        "The quick brown fox jumps over the lazy dog."
+            #endif
+                        ;
 
-        Console.WriteLine(@""
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Quisque ut sem massa.
-            In at fringilla ipsum.
-            Phasellus ut urna pretium felis porttitor euismod mattis sed augue."");
-    }
-}";
+                    Console.WriteLine(@"
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Quisque ut sem massa.
+                        In at fringilla ipsum.
+                        Phasellus ut urna pretium felis porttitor euismod mattis sed augue.");
+                }
+            }
+
+            """;
 
         var options = MinificationOptions.Default.FilterCommentMatching(@"^(//!|/\*!)");
         var minified = Minifier.Minify(NormalizeLineEndings(source), "\n", options)
@@ -221,58 +225,60 @@ static class Program
     [Test]
     public void MinifyWhileKeepingLeadMultiLineComment()
     {
-        const string source = @"
-/* https://unlicense.org/
- *
- * This is free and unencumbered software released into the public domain.
- *
- * Anyone is free to copy, modify, publish, use, compile, sell, or
- * distribute this software, either in source code form or as a compiled
- * binary, for any purpose, commercial or non-commercial, and by any
- * means.
- *
- * In jurisdictions that recognize copyright laws, the author or authors
- * of this software dedicate any and all copyright interest in the
- * software to the public domain. We make this dedication for the benefit
- * of the public at large and to the detriment of our heirs and
- * successors. We intend this dedication to be an overt act of
- * relinquishment in perpetuity of all present and future rights to this
- * software under copyright law.
- *
- * THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- *
- * For more information, please refer to <http://unlicense.org>
- */
+        const string source = """
+            /* https://unlicense.org/
+             *
+             * This is free and unencumbered software released into the public domain.
+             *
+             * Anyone is free to copy, modify, publish, use, compile, sell, or
+             * distribute this software, either in source code form or as a compiled
+             * binary, for any purpose, commercial or non-commercial, and by any
+             * means.
+             *
+             * In jurisdictions that recognize copyright laws, the author or authors
+             * of this software dedicate any and all copyright interest in the
+             * software to the public domain. We make this dedication for the benefit
+             * of the public at large and to the detriment of our heirs and
+             * successors. We intend this dedication to be an overt act of
+             * relinquishment in perpetuity of all present and future rights to this
+             * software under copyright law.
+             *
+             * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+             * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+             * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+             * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+             * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+             * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+             * OTHER DEALINGS IN THE SOFTWARE.
+             *
+             * For more information, please refer to <http://unlicense.org>
+             */
 
-#region Imports
-using System;
-#endregion
+            #region Imports
+            using System;
+            #endregion
 
-static class Program
-{
-    static void Main()
-    {
-        const string alphabetText =
-#if UPPER
-            ""THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.""
-#else // !UPPER
-            ""The quick brown fox jumps over the lazy dog.""
-#endif
-            ;
+            static class Program
+            {
+                static void Main()
+                {
+                    const string alphabetText =
+            #if UPPER
+                        "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."
+            #else // !UPPER
+                        "The quick brown fox jumps over the lazy dog."
+            #endif
+                        ;
 
-        Console.WriteLine(@""
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Quisque ut sem massa.
-            In at fringilla ipsum.
-            Phasellus ut urna pretium felis porttitor euismod mattis sed augue."");
-    }
-}";
+                    Console.WriteLine(@"
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Quisque ut sem massa.
+                        In at fringilla ipsum.
+                        Phasellus ut urna pretium felis porttitor euismod mattis sed augue.");
+                }
+            }
+
+            """;
 
         var options = MinificationOptions.Default.WithKeepLeadComment(true);
         var minified = Minifier.Minify(NormalizeLineEndings(source), "\n", options)
@@ -331,60 +337,62 @@ static class Program
     [Test]
     public void MinifyWhileKeepingLeadSingleLineComments()
     {
-        const string source = @"
-// https://unlicense.org/
-//
-// This is free and unencumbered software released into the public domain.
-//
-// Anyone is free to copy, modify, publish, use, compile, sell, or
-// distribute this software, either in source code form or as a compiled
-// binary, for any purpose, commercial or non-commercial, and by any
-// means.
-//
-// In jurisdictions that recognize copyright laws, the author or authors
-// of this software dedicate any and all copyright interest in the
-// software to the public domain. We make this dedication for the benefit
-// of the public at large and to the detriment of our heirs and
-// successors. We intend this dedication to be an overt act of
-// relinquishment in perpetuity of all present and future rights to this
-// software under copyright law.
-//
-// THE SOFTWARE IS PROVIDED ""AS IS"", WITHOUT WARRANTY OF ANY KIND,
-// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-// IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-// OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-// ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-// OTHER DEALINGS IN THE SOFTWARE.
-//
-// For more information, please refer to <http://unlicense.org>
+        const string source = """
+            // https://unlicense.org/
+            //
+            // This is free and unencumbered software released into the public domain.
+            //
+            // Anyone is free to copy, modify, publish, use, compile, sell, or
+            // distribute this software, either in source code form or as a compiled
+            // binary, for any purpose, commercial or non-commercial, and by any
+            // means.
+            //
+            // In jurisdictions that recognize copyright laws, the author or authors
+            // of this software dedicate any and all copyright interest in the
+            // software to the public domain. We make this dedication for the benefit
+            // of the public at large and to the detriment of our heirs and
+            // successors. We intend this dedication to be an overt act of
+            // relinquishment in perpetuity of all present and future rights to this
+            // software under copyright law.
+            //
+            // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+            // EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+            // MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+            // IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+            // OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+            // ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+            // OTHER DEALINGS IN THE SOFTWARE.
+            //
+            // For more information, please refer to <http://unlicense.org>
 
-// This comment with a blank line before should not appear as part of
-// the lead comment!
+            // This comment with a blank line before should not appear as part of
+            // the lead comment!
 
-#region Imports
-using System;
-#endregion
+            #region Imports
+            using System;
+            #endregion
 
-static class Program
-{
-    static void Main()
-    {
-        const string alphabetText =
-#if UPPER
-            ""THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG.""
-#else // !UPPER
-            ""The quick brown fox jumps over the lazy dog.""
-#endif
-            ;
+            static class Program
+            {
+                static void Main()
+                {
+                    const string alphabetText =
+            #if UPPER
+                        "THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG."
+            #else // !UPPER
+                        "The quick brown fox jumps over the lazy dog."
+            #endif
+                        ;
 
-        Console.WriteLine(@""
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            Quisque ut sem massa.
-            In at fringilla ipsum.
-            Phasellus ut urna pretium felis porttitor euismod mattis sed augue."");
-    }
-}";
+                    Console.WriteLine(@"
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Quisque ut sem massa.
+                        In at fringilla ipsum.
+                        Phasellus ut urna pretium felis porttitor euismod mattis sed augue.");
+                }
+            }
+
+            """;
 
         var options = MinificationOptions.Default.WithKeepLeadComment(true);
         var minified = Minifier.Minify(NormalizeLineEndings(source), "\n", options)
